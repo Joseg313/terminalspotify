63df34ee6fd170e021188d857aeda4d5dc97c251
Jose <501jag3@gmail.com>
2026-06-27 00:18:40 -0500

    tinkering

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@ int main() {
     std::string command {};
     std::getline(std::cin >> std::ws, command);
     std:: cout << command << '\n';
-    std::cout << song_name() << '\n';
+    // get acess token using POST
+    std::cout << get_token() << '\n';
+    // std::cout << song_name() << '\n';
     return 0;
 
 }
