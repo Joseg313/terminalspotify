440c6e4227ae276cc79bcaec70f0a7abc10e3ab9
Jose <501jag3@gmail.com>
2026-07-10 21:25:50 -0500

    fixing stuff

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include "commands.h"
+#include <string>
 int main() {
-    std::cout << "hello world\n";
+    std::cout << "Enter Command: ";
+    std::string command {};
+    std::getline(std::cin >> std::ws, command);
+    std:: cout << command << '\n';
+    // get acess token using POST
+    std::cout << get_token() << '\n';
     std::cout << song_name() << '\n';
     return 0;
 
